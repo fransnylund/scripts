4ab01279f09b17fae0893a52266bab72811b34c0
tux <tux@blueberry>
2022-05-12 16:46:39 +0300

    Added script

---
+++ dotfile-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -s $HOME/dotfiles/.bashrc ~/.bashrc
+ln -s $HOME/dotfiles/.emacs ~/.emacs
+ln -s $HOME/dotfiles/.inputrc ~/.inputrc
+ln -s $HOME/dotfiles/.profile ~/.profile
+ln -s $HOME/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s $HOME/dotfiles/.vimrc ~/.vimrc
+ln -s $HOME/dotfiles/.xinitrc ~/.xinitrc
+ln -s $HOME/dotfiles/.xmobarrc ~/.xmobarrc
+ln -s $HOME/dotfiles/.xprofile ~/.xprofile
+ln -s $HOME/dotfiles/.Xresource ~/.Xresource
+ln -s $HOME/dotfiles/.zshrc ~/.zshrc
+
